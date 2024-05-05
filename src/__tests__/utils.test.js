4d209b84ceb922a954b0fc88e5b9b1f3ef9164df
Calvin Chege <calvinchege1@gmail.com>
2024-05-05 17:59:27 +0300

    complete lab

--- src/__tests__/utils.test.js
+++ src/__tests__/utils.test.js
@@ -1,1 +1,19 @@
-// Your tests here
+import {isPalindrome} from "../utils"
+
+describe ("isPalindrome",()=>{
+    it("should return true if a word is a palindrome",()=>{
+        expect(isPalindrome("racecar")).toBe(true)
+    })
+
+    it("should return false if a word is not a palindrome",()=>{
+        expect(isPalindrome("car")).toBe(false)
+    })
+
+    it("should return true if a palindrome is a combination of uppercase and lowercase letters",()=>{
+        expect(isPalindrome("RacEcAr")).toBe(true)
+    })
+
+    it("should return false if input is an empty string",()=>{
+        expect(isPalindrome("")).toBe(false)
+    })
+})
--- src/utils.js
+++ src/utils.js
@@ -1,1 +1,14 @@
-// Your code here
+export function isPalindrome(word) {
+    if (typeof word !== 'string') {
+      throw new Error("Input must be a string")
+    }
+    
+    if (word === "") {
+      return false
+    }
+    
+    const lowercaseWord = word.toLowerCase()
+    const reversedWord = lowercaseWord.split('').reverse().join('');
+
+    return lowercaseWord === reversedWord;
+  }
